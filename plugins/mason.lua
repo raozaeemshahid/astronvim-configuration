fbf8209f83e3c35968b10a7f43f556f6a0694c38
Rao Zaeem Shahid <raozaeem2018@gmail.com>
2023-12-28 12:59:14 +0500

    added discord presence

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -4,8 +4,8 @@ return { -- use mason-lspconfig to configure LSP installations
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-        ensure_installed = {"lua_ls", "clangd", "cssls", "cssmodules_ls", "unocss", "html", "tsserver", "prismals",
-                            "jedi_language_server", "tailwindcss", "rust_analyzer"}
+        ensure_installed = {"lua_ls", "cssls", "cssmodules_ls", "unocss", "html", "tsserver", "prismals",
+                            "jedi_language_server", "tailwindcss"}
     }
 }, -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
 {
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = { "lua", "typescript", "javascript", "python", "prisma", "html" },
-  },
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = {"lua", "typescript", "javascript", "python", "prisma", "html"}
+    }
 }
