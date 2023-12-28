45236e46eec17ec349f9580a0ecc56e79ac5bc15
Rao Zaeem Shahid <raozaeem2018@gmail.com>
2023-12-28 15:27:44 +0500

    Copy to clipboard ensured

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ return {
   -- Set colorscheme to use
   colorscheme = "astrodark",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+  clipboard = "unnamedplus",
   diagnostics = {
     virtual_text = true,
     underline = true,
