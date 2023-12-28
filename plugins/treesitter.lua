a559bef35b162fab5ce46a77868c778dd2914289
Rao Zaeem Shahid <raozaeem2018@gmail.com>
2023-12-28 15:27:49 +0500

    reformatted

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-        ensure_installed = {"lua", "typescript", "javascript", "python", "prisma", "html"}
+        ensure_installed = {"lua", "typescript", "javascript", "python", "prisma", "html", "jsonc" }
     }
 }
