813f004dea7af3a66b3f6ee3c7c85c7d99934533
Rao Zaeem Shahid <raozaeem2018@gmail.com>
2024-01-27 20:50:16 +0500

    Formatter added

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,23 +1,34 @@
 -- customize mason plugins
 return { -- use mason-lspconfig to configure LSP installations
-{
+  {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-        ensure_installed = {"lua_ls", "cssls", "cssmodules_ls", "unocss", "html", "tsserver", "prismals",
-                            "jedi_language_server", "tailwindcss"}
-    }
-}, -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-{
+      ensure_installed = {
+        "lua_ls",
+        "cssls",
+        "cssmodules_ls",
+        "unocss",
+        "html",
+        "tsserver",
+        "prismals",
+        "jedi_language_server",
+        "tailwindcss",
+      },
+    },
+  }, -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-        ensure_installed = {"prettier"}
-    }
-}, {
+      ensure_installed = { "prettier", "stylua" },
+    },
+  },
+  {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-        ensure_installed = {"python"}
-    }
-}}
+      ensure_installed = { "python" },
+    },
+  },
+}
