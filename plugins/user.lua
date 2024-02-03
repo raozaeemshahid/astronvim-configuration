2f42847db33f2b58175c44578804d5af54fe01b8
Rao Zaeem Shahid <raozaeem2018@gmail.com>
2024-02-03 14:09:04 +0500

    Better TS errors added

--- plugins/user.lua
+++ plugins/user.lua
@@ -84,4 +84,14 @@ return { -- You can also add new plugins here as well:
     "cseickel/diagnostic-window.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
   },
+  {
+    "OlegGulevskyy/better-ts-errors.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = {
+      keymaps = {
+        toggle = "<leader>dd",           -- default '<leader>dd'
+        go_to_definition = "<leader>dx", -- default '<leader>dx'
+      },
+    },
+  },
 }
