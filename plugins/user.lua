5ccc8e809da814f346618eee517fb1ca09c37807
Rao Zaeem Shahid <raozaeem2018@gmail.com>
2024-02-06 11:47:46 +0500

    LSP hover removed

--- plugins/user.lua
+++ plugins/user.lua
@@ -50,37 +50,6 @@ return { -- You can also add new plugins here as well:
   { "hrsh7th/cmp-cmdline" },
   { "saadparwaiz1/cmp_luasnip" },
   {
-    "lewis6991/hover.nvim",
-    config = function()
-      require("hover").setup {
-        init = function()
-          -- Require providers
-          require "hover.providers.lsp"
-          -- require('hover.providers.gh')
-          -- require('hover.providers.gh_user')
-          -- require('hover.providers.jira')
-          -- require('hover.providers.man')
-          -- require('hover.providers.dictionary')
-        end,
-        preview_opts = {
-          border = "single",
-        },
-        -- Whether the contents of a currently open hover window should be moved
-        -- to a :h preview-window when pressing the hover keymap.
-        preview_window = false,
-        title = true,
-        mouse_providers = {
-          "LSP",
-        },
-        mouse_delay = 1000,
-      }
-
-      -- Setup keymaps
-      vim.keymap.set("n", "Z", require("hover").hover, { desc = "hover.nvim" })
-      vim.keymap.set("n", "gZ", require("hover").hover_select, { desc = "hover.nvim (select)" })
-    end,
-  },
-  {
     "cseickel/diagnostic-window.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
   },
