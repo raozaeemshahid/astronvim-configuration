44392ba71be38841e25aafdc31a2adb01126ac6c
Rao Zaeem Shahid <raozaeem2018@gmail.com>
2024-02-14 12:43:08 +0500

    better ts errors removed

--- plugins/user.lua
+++ plugins/user.lua
@@ -51,15 +51,5 @@ return {
   { "hrsh7th/cmp-path" },
   { "hrsh7th/cmp-cmdline" },
   { "saadparwaiz1/cmp_luasnip" },
-  {
-    "OlegGulevskyy/better-ts-errors.nvim",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    config = {
-      keymaps = {
-        toggle = "<leader>dd",           -- default '<leader>dd'
-        go_to_definition = "<leader>dx", -- default '<leader>dx'
-      },
-    },
-  },
   "chrisbra/NrrwRgn",
 }
